8e4c666357020e4562201ca5f562bb0fbe0dd74a
sbs <sbs>
2026-07-25 11:53:32 +0900

    oauth kakao feature

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,10 +2,15 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< Updated upstream
+=======
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+>>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BoardApplication {
 
     public static void main(String[] args) {
